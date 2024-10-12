e23c7bb7eab3f4e0ed178062d28437c4546f229d
Nabonb <nabobaruanb@gmail.com>
2024-10-13 02:40:34 +0600

    bug fixed

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD5XQbLoTuET0dUzTsXfvdaEQLkQZY2dQY",
+  authDomain: "basic-authentication-4178f.firebaseapp.com",
+  projectId: "basic-authentication-4178f",
+  storageBucket: "basic-authentication-4178f.appspot.com",
+  messagingSenderId: "86018648760",
+  appId: "1:86018648760:web:0cafd0ebcb388ac360a24d",
 };
 
 // Initialize Firebase
